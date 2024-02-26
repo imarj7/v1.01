60d47c80e5c285587cbd99ea88e65321652d56c1
Aniket Raj Jha <104718492+imarj7@users.noreply.github.com>
2024-02-26 18:36:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+function goToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' 
+    });
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("goToTopBtn").style.display = "block";
+    } else {
+        document.getElementById("goToTopBtn").style.display = "none";
+    }
+}
